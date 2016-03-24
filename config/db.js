f6db044ad0d2d1a94a39c1a913df17fa0de65817
rphovley <rphovley@gmail.com>
2016-03-24 13:38:04 -0600

    changed port and database url

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = {
-        url : 'mongodb://localhost:27017/test_gcm'
+        url : 'mongodb://MongoLab-x4:VUd4W3gl2oIIk5gdOrPLGKP80YF9a5zfrtbnu4NoAsU-@ds064278.mlab.com:64278/MongoLab-x4'
     }
