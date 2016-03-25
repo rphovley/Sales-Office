baaa877c27ae87b0101afb680f174600d4f3bcee
rphovley <rphovley@gmail.com>
2016-03-25 12:51:28 -0600

    switched 'libs' to 'bower_componenets'

---
+++ public/js/controllers/HomeCtrl.js
@@ -0,0 +1,7 @@
+'use strict';
+// public/js/controllers/HomeCtrl.js
+angular.module('HomeCtrl', []).controller('HomeController', function($scope) {
+
+    $scope.tagline = 'To the moon and back!';   
+    $scope.parent = 'home stuff';
+});
